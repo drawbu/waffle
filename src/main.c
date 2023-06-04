74849a330a480e074a829c3d4be8985a91099235
Clément <clement2104.boillot@gmail.com>
2023-06-04 19:56:01 +0200

    :tada: Add the basic example from Wikipedia

--- src/main.c
+++ src/main.c
@@ -1,11 +1,62 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "debug.h"
+#include <X11/Xlib.h>
 
-int main(int argc DEBUG_USED, char **argv DEBUG_USED)
+int main(void)
 {
-    printf("Hello!\n");
-    DEBUG("Received %d arguments.", argc);
-    return EXIT_SUCCESS;
-}
+    Display *display;
+    Window window;
+    XEvent event;
+    char *msg = "Hello, World!";
+    int s;
+ 
+    // open connection to the server
+    display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
+ 
+    s = DefaultScreen(display);
+ 
+    // create window
+    window = XCreateSimpleWindow(
+        display,
+        RootWindow(display, s),
+        10,
+        10,
+        200,
+        200,
+        1,
+        BlackPixel(display, s),
+        WhitePixel(display, s
+    ));
+ 
+    // select kind of events we are interested in
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+ 
+    // map (show) the window
+    XMapWindow(display, window);
+ 
+    // event loop
+    while (true) {
+        XNextEvent(display, &event);
+ 
+        // draw or redraw the window
+        if (event.type == Expose) {
+            XFillRectangle(display, window, DefaultGC(display, s), 20, 20, 10, 10);
+            XDrawString(display, window, DefaultGC(display, s), 50, 50, msg, strlen(msg));
+        }
+        // exit on key press
+        if (event.type == KeyPress)
+            break;
+    }
+ 
+    // close connection to the server
+    XCloseDisplay(display);
+ 
+    return 0;
+ }
