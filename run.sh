7ec5ca5e8b379b81871adbb98012e1e3c9deb666
Sigmanificient <edhyjox@gmail.com>
2023-06-05 13:35:56 +0200

    :wrench: Update launcher script

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+[ ! $# -eq 0 ] || exit
+target=$1
+
+[ ! -e $target ] && make $target
+[ -e $target ] || exit
+
+xephyr=$(ps -ef | grep Xephyr | grep -v "grep" | wc -l)
+
+if [ $xephyr -eq 0 ]
+then
+    Xephyr -br -ac -reset -screen 1280x720 :1 &
+    sleep 1s
+
+    export DISPLAY=:1
+fi
+
+./$target
 pkill -f Xephyr
-Xephyr -br -ac -reset -screen 1280x720 :1 &
-
-sleep 1s
-export DISPLAY=:1
-
-./$1
--- src/main.c
+++ src/main.c
@@ -46,8 +46,11 @@ void wm_run(Display *display)
     while (wm.is_running) {
         XNextEvent(display, &wm.event);
         event_callback = EVENT_TABLE[wm.event.type];
-        if (event_callback)
-            event_callback(&wm);
+        if (!event_callback) {
+            DEBUG("Missing callback for event [%d]", wm.event.type);
+            continue;
+        }
+        event_callback(&wm);
     }
     remove_grab(display);
 }
@@ -63,6 +66,7 @@ int main(void)
     DEBUG("Display size: [%d, %d]",
         XDisplayWidth(display, 0), XDisplayHeight(display, 0));
     wm_run(display);
+    DEBUG_MSG("Closing server");
     XCloseDisplay(display);
     return EXIT_SUCCESS;
 }
